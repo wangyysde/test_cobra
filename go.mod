517240d42a962759e770335b4986820b3caa360a
wangyysde <net_use@bzhy.com>
2021-06-21 22:34:08 +0800

    add generate example
    Signed-off-by: wangyysde <net_use@bzhy.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wangyysde/test_cobra
+
+go 1.16
+
+require github.com/spf13/cobra v1.1.3
---
+++ main.go
@@ -0,0 +1,11 @@
+
+package main
+
+import (
+
+	"github.com/wangyysde/test_cobra/cmd"		
+)
+
+func main(){
+	cmd.Execute()
+}
